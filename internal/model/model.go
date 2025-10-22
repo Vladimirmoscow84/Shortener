21e05c99fde9f6c40d99da8403a07ea1167f939c
Vladimirmoscow84 <ccr1@yandex.ru>
2025-10-22 13:20:10 +0300

    add json and db tags in model

--- internal/model/model.go
+++ internal/model/model.go
@@ -3,16 +3,16 @@ package model
 import "time"
 
 type ShortURL struct {
-	ID           uint
-	OriginalCode string
-	ShortCode    string
-	CreatedAt    time.Time
-	Clicks       []Click
+	ID           uint      `json:"id" db:"id"`
+	OriginalCode string    `json:"original_code" db:"original_code"`
+	ShortCode    string    `json:"short_code" db:"short_code"`
+	CreatedAt    time.Time `json:"created_at" db:"created_at"`
+	Clicks       []Click   `json:"clicks"`
 }
 
 type Click struct {
-	ID         uint
-	ShortURLID uint
-	UserAgent  string
-	Timestamp  time.Time
+	ID         uint      `json:"id" db:"id"`
+	ShortURLID uint      `json:"short-url_id" db:"short_url_id"`
+	UserAgent  string    `json:"user-agent" db:"user_agent"`
+	Timestamp  time.Time `json:"timestamp" db:"timestamp"`
 }
